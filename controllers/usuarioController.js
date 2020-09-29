56a33cb9074319baf8b82c60ad81049aa45e8709
LuisaIvonne <garlivon@gmail.com>
2020-09-29 12:18:35 -0500

    Hashear passwords en la base de datos

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,17 +1,32 @@
 const Usuario =  require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
 
 exports.crearUsuario = async (req, res) => {
+    //extraer email y password
+    const {email, password} = req.body;
+    
     try {
-        let usuario;
+        //Revisar que el usuario registrado sea único
+        let usuario = await Usuario.findOne({email});
+
+        if(usuario) {
+            return res.status(400).json({msg: 'El usuario ya existe'});
+        }
+
+
 
         //crea el nuevo usuario
         usuario = new Usuario(req.body);
+
+        //Hashear el password
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password =  await bcryptjs.hash(password, salt);
 
         //guardar usuario
         await usuario.save();
 
         //mensaje de confirmacion
-        res.send('Usuario creado correctamente');
+        res.json({msg: 'Usuario creado correctamente'});
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error');
